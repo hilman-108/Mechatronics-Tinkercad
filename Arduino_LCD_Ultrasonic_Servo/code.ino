c3b2930ea520538ed2f76af28dfa7e70459b7658
Hilman Asyrafi <52323792+hilman-108@users.noreply.github.com>
2024-11-22 22:16:35 +0800

    Create code.ino

---
+++ Arduino_LCD_Ultrasonic_Servo/code.ino
@@ -0,0 +1,55 @@
+#include <Servo.h>
+
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,16,2);
+
+const int TRIGPIN = 8;          
+const int ECHOPIN = 9;
+long timer;
+int jarak;
+
+ 
+Servo myservo;
+
+void setup() {
+  lcd.init();
+  lcd.clear();
+  lcd.backlight();
+  
+  pinMode(ECHOPIN, INPUT);
+  pinMode(TRIGPIN, OUTPUT);
+
+  myservo.attach(7);
+   
+}
+
+void loop() {
+  digitalWrite(TRIGPIN, LOW);                   
+  delayMicroseconds(2);
+  digitalWrite(TRIGPIN, HIGH);                  
+  delayMicroseconds(10);
+  digitalWrite(TRIGPIN, LOW);                   
+
+  timer = pulseIn(ECHOPIN, HIGH);
+  jarak = timer/58;
+  
+  lcd.setCursor(0,0);
+  lcd.print("Measurement");
+
+  
+  lcd.setCursor(0,1);
+  lcd.print("Jarak : ");
+  lcd.print(jarak);
+  lcd.print(" cm");
+
+  
+  if(jarak < 100){
+  	myservo.write(180);
+    delay(500);
+    myservo.write(-180);
+    delay(500);
+  }
+  
+}
+
